8a814929d7b59f171a3401defe22c785f1f9d524
ZephyrsSin <68823159+VenomShark@users.noreply.github.com>
2026-01-08 00:39:24 +0530

    Add files via upload

---
+++ assignment1_task1.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+#Addition
+result = num1 + num2
+print("Addition:", float(result))
+
+#Subtraction
+result = num1 - num2
+print("Subtraction:", round(float(result),2))
+
+#Multiplication
+result = num1 * num2
+print("Multiplication:", float(result))
+
+#Division
+result = num1 / num2
+print("Division:", round(result,2))
+
